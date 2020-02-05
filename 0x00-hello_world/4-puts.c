0d7bafadf65cc25daf35475f70446d4bfe1eb1a6
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-05 19:06:25 +0000

    scripts3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
+ * Description:user blabalabalabll
  * Return: Always 0 (Success)
  */
 int main(void)
-{  
-char lista []="\"Programming is like building a multilingual puzzle";
+{
+char lista[] = "\"Programming is like building a multilingual puzzle";
 puts(lista);
 return (0);
 }

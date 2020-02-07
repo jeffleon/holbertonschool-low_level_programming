d7564595843cb313f046d42d9580a1ce02fa0bcc
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-07 03:07:52 +0000

    script3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ if (nuevo > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, nuevo);
 else if (nuevo < 6)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, nuevo);
-else
+else if (nuevo == 0)
 printf("Last digit of %d is %d and and is 0\n", n, nuevo);
 return (0);
 }

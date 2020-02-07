2be02f9943ddfb152d35d64619132846f00902dc
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-07 00:21:06 +0000

    scripts 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:user blabalabalabll
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+int i, j, flag=0,flag1=0;
+for (j = 0; j < 10; j++)
+{ flag+=1;
+for (i = 0; i < 10; i++)
+{
+if (flag <= i)
+{
+flag1+=1;
+putchar(j + '0');
+putchar(i + '0');
+if(flag1!=45)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n'); return (0);
+}

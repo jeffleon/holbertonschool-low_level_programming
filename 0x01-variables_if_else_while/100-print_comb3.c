34295074cdff9f1b417d81d9960df75e0e240019
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-07 00:27:26 +0000

    scripts 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,12 @@ int main(void)
 int i, j, flag = 0, flag1 = 0;
 for (j = 0; j < 10; j++)
 {
-flag + = 1;
+flag += 1;
 for (i = 0; i < 10; i++)
 {
 if (flag <= i)
 {
-flag1 + = 1;
+flag1 += 1;
 putchar(j + '0');
 putchar(i + '0');
 if (flag1 != 45)

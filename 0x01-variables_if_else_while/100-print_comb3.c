ba95046c1c6039b9b59633f072e6476af01516c6
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-07 00:25:23 +0000

    script3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,19 +4,20 @@
  * Description:user blabalabalabll
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-int i, j, flag=0,flag1=0;
+int i, j, flag = 0, flag1 = 0;
 for (j = 0; j < 10; j++)
-{ flag+=1;
+{
+flag + = 1;
 for (i = 0; i < 10; i++)
 {
 if (flag <= i)
 {
-flag1+=1;
+flag1 + = 1;
 putchar(j + '0');
 putchar(i + '0');
-if(flag1!=45)
+if (flag1 != 45)
 {
 putchar(',');
 putchar(' ');
@@ -24,5 +25,6 @@ putchar(' ');
 }
 }
 }
-putchar('\n'); return (0);
+putchar('\n');
+return (0);
 }

ea68d658248f79ead21f39287ed9b34edf899269
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-11 18:23:29 +0000

    scripts 3

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
  * _abs - Entry point
- * @c int variable
+ * @c: int variable
  * Description:user blabalabalabll
  * Return: Always 0 (Success)
  */
 int _abs(int c)
 {
-	if (c<0)
-	return(c*-1);
+	if (c < 0)
+	return (c * -1);
 	else
-	return(c);
+	return (c);
 }
 

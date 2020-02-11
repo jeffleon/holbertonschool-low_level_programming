62261e9d577efff8a42219bf6b45db19517578f9
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-11 21:38:46 +0000

    scripts 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include "holberton.h"
 
 /**
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) || (year % 100 == 0))
 	{
-	if (month >= 2 || day >= 60)
+	if (month >= 2 && day >= 60)
 	{
 	day++;
 	}

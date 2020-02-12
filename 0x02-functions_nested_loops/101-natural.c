96e7a283a6a18998870f3ad26589341dc368c9d9
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-12 02:57:00 +0000

    scripts 3

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description:user blabalabalabll
+ *
  * Return: Always 0 (Success)
  */
 void main(void)
 {
 	int i, sum = 0;
 
-	for (i = 0; i < 1024; i++)
+	for (i = 1; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		sum = sum + i;

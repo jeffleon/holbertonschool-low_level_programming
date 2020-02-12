38d856a680fea04ec0b53a14184a97711338c86c
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-12 04:46:06 +0000

    scripts3

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description:user blabalabalabll
- * Return: Always 0 (Success)
+ * main - Get the sum of even Fibonnaci numbers up to 4 million
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
 	int a;
-	long long int flag, A_af = 1, A_be = 1, sum = 0;
+	long long int flag, A_af = 1, A_be = 0, sum = 0;
 
 	for (a = 1; a < 4000000; a++)
 	{
-		flag = A_af;
-		A_af = A_be + A_af;
-		A_be = flag;
-		if (A_af % 2 == 0)
-		{
-			sum = sum + A_af;
-		}
+	flag = A_af;
+	A_af = A_be + A_af;
+	A_be = flag;
+	if (A_af % 2 == 0)
+	{
+		sum = sum + A_af;
 	}
-	printf("%ld\n", sum);
+	}
+	printf("%lld", sum);
+	putchar('\n');
 	return (0);
 }

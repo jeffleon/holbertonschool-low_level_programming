f8b8214b3880763b8bbbe37f1c1bdb0c23543289
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-14 04:28:10 +0000

    scripts 3

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long n = 612852475143;
+	int i, primo = 1, primomax = 0, verificacion = 0;
+
+	while (n != 1)
+	{
+		n = n / primo;
+		for (i = 2; i <= n ; i++)
+		{
+			if (n % i == 0)
+			{
+				primo = i;
+				break;
+			}
+		}
+		for (i = 1; i <= primo ; i++)
+		{
+			if (primo % i == 0)
+			{
+				verificacion++;
+			}
+		}
+		if (verificacion <= 2)
+		{
+			primomax = primo;
+			verificacion = 0;
+		}
+	}
+	printf("%d\n", primomax);
+	return (0);
+}

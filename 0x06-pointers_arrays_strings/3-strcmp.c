12d167e8a3655175f6a539121b151dda6360646b
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-21 01:20:41 +0000

    fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,9 +9,9 @@ int _strcmp(char *s1, char *s2)
 {
 	int cantidad = 0, i = 0;
 
-	for (i = 0; *(s + i) != 0 && *(s2 + i) != 0; i++)
+	for (i = 0; *(s1 + i) != 0 && *(s2 + i) != 0; i++)
 	{
-		cantidad = s[i] - s2[i];
+		cantidad = s1[i] - s2[i];
 		if (cantidad == 0)
 		continue;
 		else

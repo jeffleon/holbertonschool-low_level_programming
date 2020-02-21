ef73d829301d3857e5b1f08b425a1522d716209e
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-21 03:32:35 +0000

    8-rot13.c

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,10 +1,26 @@
 #include "holberton.h"
 /**
- * reset_to_98 - check the code for Holberton School students.
- * @n: entry pointer
+ * rot13 - check the code for Holberton School students.
+ * @s: entry pointer
  * Return: Always 0.
  */
 char *rot13(char *s)
 {
+	char orgin[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char rot13[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+	int length = 0, lenrot = 0;
 
+	while (*(s + length))
+	{
+		for (lenrot = 0; *(orgin + lenrot); lenrot++)
+		{
+			if (*(s + length) == *(orgin + lenrot))
+			{
+				*(s + length) = *(rot13 + lenrot);
+				break;
+			}
+		}
+	length++;
+	}
+	return (s);
 }

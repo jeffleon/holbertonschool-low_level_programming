8f5af9c406b5ccc8f7dd7f6de8bc660fbdc16bb7
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-26 23:44:18 +0000

    scripts3

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,10 +8,10 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
+	if (y > 0)
 		return (1);
 	if (y < 0)
 		return (-1);
-	return (x * _pow_recursion(x, y--));
+	return (x * _pow_recursion(x, y-1));
 }
 

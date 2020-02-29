3935a2c4cae10e51de0e052f8465e3c619e34224
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-29 22:10:19 +0000

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) >= 0 && atoi(argv[i]))
+			if (atoi(*argv[i]) >= 0 && atoi(*argv[i]))
 				add += atoi(argv[i]);
 			else
 			{

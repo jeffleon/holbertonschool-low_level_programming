acb826ac7fa9fbd178100eeb20f0ac12775885b3
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-02-29 21:40:56 +0000

    fix the folder

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - put other points
+ * @argv: the address of memory to print
+ * @argc: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - put other points
+ * @argv: the array string
+ * @argc: array size
+ *
+ * Return: Nothing.
+*/
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+			mul *= atoi(argv[i]);
+		printf("%d\n", mul);
+	}
+	else
+	printf("Error\n");
+	return (0);
+}

2d359a4420b75d6a02e51fcb701080871001c0e0
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-04 20:18:52 +0000

    fix

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,22 +1,33 @@
 #include "holberton.h"
-
 /**
- * strtow - splits a string into words
+ * palabras - splits a string into words
  * @str: given string
  *
  * Return: a pointer to an array of strings (words)
  * NULL if str == NULL or str == ""
  */
-char **strtow(char *str)
+int palabras(char *str)
 {
-	int countwords = 0, i = 0, palgran = 0, pal = 0, j = 0, k = 0, p = -1, m = 0;
-	char **output;
+	int countwords = 0, i = 0;
 
 	for (i = 0; str[i]; i++)
 	{
 		if (str[i] != ' ' && str[i + 1] == ' ')
 			countwords++;
 	}
+	return (countwords);
+}
+/**
+ * letras - splits a string into words
+ * @str: given string
+ *
+ * Return: a pointer to an array of strings (words)
+ * NULL if str == NULL or str == ""
+ */
+int letras(char *str)
+{
+	int j = 0, pal = 0, palgran = 0;
+
 	for (j = 0; str[j]; j++)
 	{
 		if (str[j] != ' ')
@@ -30,9 +41,38 @@ char **strtow(char *str)
 			pal = 0;
 		}
 	}
+	return (palgran);
+}
+/**
+ * strtow - splits a string into words
+ * @str: given string
+ *
+ * Return: a pointer to an array of strings (words)
+ * NULL if str == NULL or str == ""
+ */
+char **strtow(char *str)
+{
+	int countwords = 0, i = 0, palgran = 0, k = 0, p = -1, m = 0;
+	char **output;
+
+	countwords = palabras(str);
+	palgran = letras(str);
+	if (str == NULL)
+		return (NULL);
 	output = malloc(1 + countwords * sizeof(char *));
+	if (output == NULL)
+		return (NULL);
 	for (i = 0; i < countwords; i++)
+	{
 		output[i] = malloc((palgran + countwords) * sizeof(char));
+		if (output[i] == NULL)
+		{
+			while (--i)
+				free(output[i]);
+			free(output);
+			return (NULL);
+		}
+	}
 	for (k = 0; str[k]; k++)
 	{
 		if (str[k] != ' ')

9adab66ad73b98c0acff4b3c9780686090a3f88f
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-04 00:58:45 +0000

    scripts

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	if (size == 0)
-		return (0);
+		return (NULL);
 	point = malloc(size);
 	if (point == NULL)
-		return (0);
+		return (NULL);
 	for (i = 0; i < size; i++)
-	point[i] = c;
+		point[i] = c;
 	return (point);
 }

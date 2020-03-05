b521b361cc7c9ddb5ea211271a8dc08826f78a59
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-05 23:38:49 +0000

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,13 +11,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int count = 0, i = 0, count1 = 0;
 	char *firword;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (count = 0; s1[count]; count++)
 	{}
 	for (count1 = 0; s2[count1]; count1++)
 	{}
-	if (count1 > n)
-		count1 = n;
-	firword = malloc((count + count1 + 1) * sizeof(char));
+	if (n > count1)
+		n = count1;
+	firword = malloc((count + n + 1) * sizeof(char));
 	if (firword == NULL)
 		return (NULL);
 	for (i = 0; *s1; i++)
@@ -25,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
                 firword[i] = *s1;
                 s1++;
         }
-        for (; i < count + count1; i++)
+        for (; i < count + n; i++)
         {
                 firword[i] = *s2;
                 s2++;

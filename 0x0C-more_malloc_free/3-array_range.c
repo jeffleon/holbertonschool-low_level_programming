292f3f8f7837db827692d18d05ea12b5a1db9d39
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-05 23:14:24 +0000

    fixed

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,6 +11,10 @@ int *array_range(int min, int max)
 {
 	int *numeros = malloc(sizeof(int) * (max - min) + 1), i = 0;
 
+	if (min > max)
+		return (NULL);
+	if (numeros == NULL)
+		return (NULL);
 	for (i = min; i < max + 1; i++)
 		numeros[i - min] = i;
 	return (numeros);

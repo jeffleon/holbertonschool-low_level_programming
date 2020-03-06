b70b2c839209894118695b419a6d34d813e54930
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-06 00:11:11 +0000

    fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,15 +1,31 @@
 #include "holberton.h"
 /**
- * string_nconcat - put other points
- * @s1: 6-6-6 the number of the best
- * @s2: the argv array
- * @n: the number
+ * _realloc - put other points
+ * @ptr: 6-6-6 the number of the best
+ * @old_size: the argv array
+ * @new_size: the number
  * Return: Nothing.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	free(ptr);
-	ptr = malloc(sizeof(char) * new_size);
+	if (new_size == old_size)
+		return (ptr);
+	else if (ptr == NULL)
+	{
+		ptr = malloc(sizeof(char) * new_size);
+		return (ptr);
+	}
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (new_size > old_size)
+	{
+		free(ptr);
+		ptr = malloc(sizeof(char) * new_size);
+		return (ptr);
+	}
 	return (ptr);
 }
 

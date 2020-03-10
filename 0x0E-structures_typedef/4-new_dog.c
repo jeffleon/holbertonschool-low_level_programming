d5d6b97c43de48a5c9083c2c0035ef1ab9a6c748
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-10 01:23:23 +0000

    struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,9 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "dog.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
  *_strdup - returns a pointer to a newly allocated space in memory, which
  *contains a copy of the string given as a parameter

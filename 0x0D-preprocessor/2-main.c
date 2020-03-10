5b86ee526321e99d90d71fbd052674063681f9c4
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-10 13:59:16 +0000

    fixed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,14 +2,12 @@
 
 /**
  * main - put other points
- * @width: the address of memory to print
- * @height: the size of the memory to print
  *
  * Return: Nothing.
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-        return (0);
+	return (0);
 }
 

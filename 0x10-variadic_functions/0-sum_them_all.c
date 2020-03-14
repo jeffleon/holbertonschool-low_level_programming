734f8dd5b04634d8702bc14539069bfbd9139beb
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-14 21:21:43 +0000

    scripts

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,7 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(arguments, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(arguments, unsigned int);
+		sum += va_arg(arguments, int);
 	}
 	va_end(arguments);
 	return (sum);

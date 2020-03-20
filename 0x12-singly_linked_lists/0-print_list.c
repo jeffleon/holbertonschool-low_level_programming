608f1afa8d1b797fb4764239140dee572856cf37
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-20 01:59:52 +0000

    sctipr

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,20 +1,25 @@
 #include "lists.h"
-
+/**
+ * print_list - singly linked list
+ * @h: string - (malloc'ed string)
+ *
+ * Return: a number
+ */
 size_t print_list(const list_t *h)
 {
 	const list_t *copia;
 	size_t i = 0;
 
 	copia = h;
-	while(copia)
+	while (copia)
 	{
 		if (!copia->str)
-                	printf("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", copia->len, copia->str);
 
 		copia = copia->next;
 		i++;
 	}
-	return(i);
+	return (i);
 }

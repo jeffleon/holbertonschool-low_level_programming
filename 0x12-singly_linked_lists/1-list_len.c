577144d90ce4d20ee6020a13652271bdd6fc9884
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-20 01:45:47 +0000

    scripts

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 
 size_t list_len(const list_t *h)
 {
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,8 +1,21 @@
-#include "list.h"
+#include "lists.h"
 
 list_t *add_node(list_t **head, const char *str)
 {
-	
+	list_t *new_node = NULL;
+	new_node = malloc(sizeof(list_t));
+	unsigned int length = 0;
 
+	if (!new_node)
+		return (NULL);
+	if (!head)
+		return (NULL);
+	while (str[length])
+	length++;
 
+	new_node->str = strdup(str);
+	new_node->len = length;
+	new_node->next = *head;
+	*head = new_node;
+	return(*head);
 }

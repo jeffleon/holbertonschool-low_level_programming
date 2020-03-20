f2b380cebcfa6cace48192cea37f49ba7ee3ad96
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-20 04:03:40 +0000

    sctiprs

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+/**
+ *thisbeforemain - function that prints You're beat! and yet, you must allow,
+ *\nI bore my house upon my back!\n before the main function is executed.
+ *
+ *Return: 0
+ */
+void __attribute__((constructor)) thisbeforemain(void);
 
-void beforeMain (void) __attribute__((constructor));
-
-void beforeMain (void)
+void thisbeforeMain(void)
 {
-  printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }

a034fbf8a9b8c22738509c292e5b31ea9a59a8be
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-20 02:03:39 +0000

    scripts

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,15 +1,21 @@
 #include "lists.h"
-
+/**
+ * list_len - singly linked list
+ * @h: string - (malloc'ed string)
+ *
+ * Return: a number
+ */
 size_t list_len(const list_t *h)
 {
 	const list_t *copia;
 	unsigned int i = 0;
 
-	for(i = 0; copia != NULL; i++)
+	copia = h
+	while (copia)
 	{
 		i++;
 		copia = copia->next;
 	}
-	return(i);
+	return (i);
 
 }

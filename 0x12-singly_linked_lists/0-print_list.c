ebbe8ffd62b913e7284ca1f4857b0949fde8ab40
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-20 01:55:17 +0000

    scripts

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,11 +6,13 @@ size_t print_list(const list_t *h)
 	size_t i = 0;
 
 	copia = h;
-	if (!copia->str)
-		printf("[0] (nil)");
-	while(copia != NULL)
+	while(copia)
 	{
-		printf("[%d] %s\n", copia->len, copia->str);
+		if (!copia->str)
+                	printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", copia->len, copia->str);
+
 		copia = copia->next;
 		i++;
 	}

09bf007464106eb88b20e6c4723abe4d419e9d0e
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-20 02:16:54 +0000

    sctipr

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,8 +3,8 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = NULL;
+	size_t length = 0;
 	new_node = malloc(sizeof(list_t));
-	unsigned int length = 0;
 
 	if (!new_node)
 		return (NULL);
@@ -17,5 +17,5 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->len = length;
 	new_node->next = *head;
 	*head = new_node;
-	return(*head)
+	return(*head);
 }

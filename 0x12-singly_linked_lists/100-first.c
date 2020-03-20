8a0a35d34e729cacbc790aebacf4b4c50af187fc
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-20 04:05:08 +0000

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *thisbeforemain - function that prints You're beat! and yet, you must allow,
- *\nI bore my house upon my back!\n before the main function is executed.
+ * thisbeforeMain - function that prints You're beat! and yet, you must allow,
  *
- *Return: 0
+ *
+ * Return: 0
  */
 void __attribute__((constructor)) thisbeforemain(void);
 

a31f3a2f7ce3527b79c4fe66845b70692f15b5f0
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-24 16:52:40 +0000

    sctiprs

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,12 +11,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *copia = *head;
 	listint_t *new_element = malloc(sizeof(listint_t));
 
-	if (head == NULL)
+	if (head == NULL && idx != 0)
 		return (NULL);
 	if (new_element == NULL)
 		return (NULL);
 	new_element->n = n;
 	new_element->next = NULL;
+	if (idx == 0)
+	{
+		new_element->next = *head;
+		*head = new_element;
+		return (new_element)
+
+	}
 	for (idx = idx - 1; copia && idx > 0; idx--)
 		copia = copia->next;
 	new_element->next = copia->next;

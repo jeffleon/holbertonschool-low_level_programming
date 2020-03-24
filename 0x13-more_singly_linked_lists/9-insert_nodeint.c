06db7e9ad416b492b8f5b97bc4d139857ddadab0
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-24 16:55:21 +0000

    fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_element->next = *head;
 		*head = new_element;
-		return (new_element)
+		return (new_element);
 
 	}
 	for (idx = idx - 1; copia && idx > 0; idx--)

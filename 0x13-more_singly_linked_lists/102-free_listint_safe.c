c04f367a07531e9657970d20865ba0356880e476
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-25 02:04:50 +0000

    scitp

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 /**
- * find_listint_loop - singly linked list
- * @head: the head of linked list - (malloc'ed string)
+ * free_listint_loop - singly linked list
+ * @h: the head of linked list - (malloc'ed string)
  *
  * Return: a counter
  */
 size_t free_listint_safe(listint_t **h)
 {
-	const listint_t *no_loop = head;
-	const listint_t *no_loop2;
+	listint_t *no_loop = *h;
+	listint_t *no_loop2;
 	size_t count = 0;
 
-	if (no_loop == NULL || head == NULL)
+	if (no_loop == NULL || h == NULL)
 		return (0);
 	while (no_loop)
 	{

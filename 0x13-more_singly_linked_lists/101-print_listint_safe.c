841ea7e83048ee7d3ca3cd49bd4420f69bd028b0
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-25 01:19:48 +0000

    scripts

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * print_listint_safe - singly linked list
+ * @head: the head of linked list - (malloc'ed string)
+ *
+ * Return: a counter
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint *no_loop = head;
+	const listint *no_loop2;
+	size_t count = 0;
+
+	while (no_loop)
+	{
+		no_loop2 = no_loop;
+		no_loop = no_loop->next;
+		printf("[%p] %d", (*void) no_loop2, no_loop2->n);
+		count++;
+		if (no_loop >= no_loop2)
+		{
+			printf("-> [%p] %d", (*void) no_loop2->next, no_loop2->next->n);
+			break;
+		}
+	}
+	return (count);
+}

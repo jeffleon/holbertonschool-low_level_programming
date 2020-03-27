e2ffc7eb2f0ed60f27b1af16ffd008a859506194
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-03-27 03:39:56 +0000

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,30 +7,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int count = 1, i = 0;
+	int i = 0, expo = 1;
 	unsigned int numero = 0;
 
 	if (b == NULL)
 		return (0);
 	while (b[i])
 	{
-		if (b[i] != '0' || b[i] != '1')
-			return (0);
 		i++;
 	}
 	i--;
-	count = count << i;
-	i = 0;
-	while (b[i])
+	for (; i >= 0; i--)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] == '0' || b[i] == '1')
 		{
 			if (b[i] == '1')
-				numero += count;
-			else if (b[i] != '0')
-				return (0);
-			i++;
-			count = count >> 1;
+				numero += expo;
+			expo *= 2;
 		}
 		else
 			return (0);

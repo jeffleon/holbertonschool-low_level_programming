a1b0c046b61aa51b49533077ecfa96818e9cafd7
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-04-01 01:46:40 +0000

    scripts

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,11 +11,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (fd);
-
 	while (text_content[count])
 		count++;
 	write(fd, text_content, count);

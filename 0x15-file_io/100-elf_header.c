ec270f64137bc8b5297eb3c7b39216d190206626
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-04-01 03:52:48 +0000

    :)

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * main - entry point
+ * @argc: the number of arguments.
+ * @argv: arguments.
+ * Description:  description
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}

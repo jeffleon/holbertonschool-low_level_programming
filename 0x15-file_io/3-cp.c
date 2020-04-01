f03be84d655e7dd6ae56530a4af2ea465923116c
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-04-01 03:42:55 +0000

    scripts

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,17 +15,17 @@ int main(int argc, char **argv)
 	fs = open(argv[1], O_RDONLY);
 	if (fs == -1)
 		dprintf(STDERR_FILENO, ERROR98, argv[1]), exit(98);
-	fd = open(argv[2], (O_CREAT | O_WRONLY | O_TRUNC), 0664);
+	fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd == -1)
 		dprintf(STDERR_FILENO, ERROR98, argv[1]), exit(98);
-	while ((data = read(fs, buffer, 1024)))
+	while ((data = read(fs, buffer, 1024)) > 0)
 	{
-		if (data == -1)
-			dprintf(STDERR_FILENO, ERROR98, argv[1]), exit(98);
 		wrote = write(fd, buffer, data);
 		if (wrote == -1)
 			dprintf(STDERR_FILENO, ERROR99, argv[2]), exit(99);
 	}
+	if (data == -1)
+		dprintf(STDERR_FILENO, ERROR98, argv[1]), exit(98);
 	if (close(fd))
 		dprintf(STDERR_FILENO, ERROR100, fd), exit(100);
 	if (close(fs))

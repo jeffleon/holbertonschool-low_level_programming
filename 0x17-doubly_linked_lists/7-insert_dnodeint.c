0593ff8c52d7ce5bb2e1b8c5a349202aea2c505b
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-02 20:14:36 +0000

    fix the 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,21 @@
 #include "lists.h"
 /**
+ * dlistint_len - singly linked list
+ * @h: string - (malloc'ed string)
+ * Return: a number
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *copy = h;
+	size_t i = 0;
+
+	if (copy == NULL)
+		return (0);
+	for (; copy; copy = copy->next, i++)
+	{}
+	return (i);
+}
+/**
  * insert_dnodeint_at_index - singly linked list
  * @h: string - (malloc'ed string)
  * @idx: pointer
@@ -12,9 +28,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *copy_p;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	unsigned int i = 0;
+	size_t length = 0;
 
+	length = dlistint_len(*h);
+	if (idx > length)
+		return (NULL);
 	if (h == NULL && idx != 0)
 		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	if (length == idx)
+		return (add_dnodeint_end(h, n));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
@@ -24,12 +48,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		copy_p = copy_n;
 	new_node->next = copy_n;
 	copy_n->prev = new_node;
-	if (idx == 0)
-		*h = new_node;
-	else
-	{
-		copy_p->next = new_node;
-		new_node->prev = copy_p;
-	}
+	copy_p->next = new_node;
+	new_node->prev = copy_p;
 	return (*h);
 }

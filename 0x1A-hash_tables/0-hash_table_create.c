7454ead1eba2ee21963c69306eb004aaa593acf2
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-30 04:07:01 +0000

    fix 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	one->size = size;
-	one->array = malloc(sizeof(hash_node_t *), size);
+	one->array = malloc(sizeof(hash_node_t *) * size);
 	if (one->array == NULL)
 		return (NULL);
 	return (one);

167be6d780808ad93478abdcdbaeabcc25c0b76d
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-30 03:55:36 +0000

    fix 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned int i = 0;
 	hash_node_t *copy = NULL;
 
+	if (ht == NULL || key == NULL || ht->array == NULL ||
+	    ht->size == 0 || strlen(key) == 0)
+		return (NULL);
+
 	for (i = 0; i < ht->size; i++)
 	{
 		copy = ht->array[i];

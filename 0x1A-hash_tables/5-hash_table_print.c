e3c7ed98e117bd1c7e442ffa1038f5274f228d96
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-30 04:02:34 +0000

    fix 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,6 +11,8 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *copy = NULL;
 	char *token = "";
 
+	if (ht == NULL || ht->array == NULL)
+		return;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{

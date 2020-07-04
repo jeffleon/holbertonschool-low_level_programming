cc42bd1bebeaea12f7fcb3f373a5d63af89e09b7
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-07-04 05:36:20 +0000

    add 5 task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,11 +2,17 @@
 """
 5-main
 """
-perimetro = 0
-list_para = [[-1, 0],[1,0],[0,-1],[0,1]]
-for count,row in enumerate(grid):
-    for ele in range(len(row)):
-        if grid[count][ele] == 1:
-            for un,do in list_para:
-                if grid[count + un][ele + do] == 0:
+def island_perimeter(grid):
+    perimetro = 0
+    for count,row in enumerate(grid):
+        for ele in range(len(row)):
+            if grid[count][ele] == 1:
+                if count > 0 and grid[count - 1][ele] == 0:
                     perimetro+=1
+                if count < (len(grid) - 1) and grid[count + 1][ele] == 0:
+                    perimetro+=1
+                if ele > 0 and grid[count][ele - 1] == 0:
+                    perimetro+=1
+                if ele < (len(row) - 1) and grid[count][ele + 1] == 0:
+                    perimetro+=1
+    return perimetro

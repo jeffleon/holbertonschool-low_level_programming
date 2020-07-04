821ae896023704db86e9a242668cacabdd500973
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-07-04 05:40:08 +0000

    fix 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,17 +2,22 @@
 """
 5-main
 """
+
+
 def island_perimeter(grid):
     perimetro = 0
-    for count,row in enumerate(grid):
+
+    if not isinstance(grid, list):
+        return 0
+    for count, row in enumerate(grid):
         for ele in range(len(row)):
             if grid[count][ele] == 1:
                 if count > 0 and grid[count - 1][ele] == 0:
-                    perimetro+=1
+                    perimetro += 1
                 if count < (len(grid) - 1) and grid[count + 1][ele] == 0:
-                    perimetro+=1
+                    perimetro += 1
                 if ele > 0 and grid[count][ele - 1] == 0:
-                    perimetro+=1
+                    perimetro += 1
                 if ele < (len(row) - 1) and grid[count][ele + 1] == 0:
-                    perimetro+=1
+                    perimetro += 1
     return perimetro

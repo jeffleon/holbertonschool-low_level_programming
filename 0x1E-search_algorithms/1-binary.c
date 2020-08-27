75942c3b2ff596b72627b07178adf246a16c2632
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-27 10:35:27 -0400

    [UPDATE] task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,13 +1,16 @@
 #include "search_algos.h"
+
 /**
- * printarray - print a array
- * array: is a entry point
- * left: side to print
- * rigth: side to print
+ * printarray - is a function to print the array to search
+ * @array: is a pointer to the first element of the array to search in
+ * @left: is the index to begin to print
+ * @rigth: is the index to end to print
  */
+
 void printarray(int *array, int left, int rigth)
 {
   char sep = '\0';
+  
   printf("Searching in array:");
   for(;left <= rigth; left++)
   {
@@ -19,27 +22,33 @@ void printarray(int *array, int left, int rigth)
 }
 
 /**
- * binary_search - find in array
- * array: entry point
- * size: the size of the array
- * value: the value to find
- * Return: the index of the value
+ * binary_search - its a function that searches for a value in a sorted array
+ * @array: its a array to search
+ * @size: its the number of elements in array
+ * @value: its the value to search for
+ *
+ * Return: -1 if value is not present in array or if array is NULL, otherwise
+ * the first index where value is located
  */
+
 int binary_search(int *array, size_t size, int value)
 {
   int left = 0;
   int rigth = size - 1;
   int mid = 0;
-  while(left <= rigth)
+  
+  if (array == NULL)
+    return (-1);
+  while (left <= rigth)
   {
     printarray(array, left, rigth);
     mid = (left + rigth)/2;
     if (array[mid] == value)
-      return(mid);
+      return (mid);
     else if (array[mid] < value)
       left = mid + 1;
     else if (array[mid] > value)
       rigth = mid -1;
   }
-  return(-1);
+  return (-1);
 }
